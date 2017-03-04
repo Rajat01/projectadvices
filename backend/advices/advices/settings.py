bf81be5113267f96850c8c3e51d23da0981b44a6
Rajat Bhardwaj <rajatbhardwaj@blackbuck.com>
2017-03-04 15:46:17 +0530

    localhost allowed

--- backend/advices/advices/settings.py
+++ backend/advices/advices/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i5f6ygc75gsl=c8-ss*sdciv)^-2+pe7%qd%&l#zj_o3t&f67e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '192.168.0.14', '127.0.0.1', '10.0.2.2']
+ALLOWED_HOSTS = ['0.0.0.0', '192.168.0.14', '127.0.0.1', '10.0.2.2', 'localhost']
 
 
 # Application definition
